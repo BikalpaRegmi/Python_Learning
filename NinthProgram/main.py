61cdbe48ff1d02a861959ce8f2a2c713f87240e7
Bikalpa Regmi <bikalparegmihtd3@gmail.com>
2026-01-05 12:54:42 +0545

    learned Modules

---
+++ NinthProgram/main.py
@@ -0,0 +1,10 @@
+################################### Module ######################################
+
+
+import demoModule;
+
+print(demoModule.add(6,2));
+print(demoModule.sub(6,2));
+print(demoModule.mul(6,2));
+print(demoModule.div(6,2));
+
