053d1bbff7005a0a3ae4347a0300a2a53d0a59d1
Bikalpa Regmi <bikalparegmihtd3@gmail.com>
2026-01-18 05:43:52 +0545

    Removed comment

--- Seventeenth.py
+++ Seventeenth.py
@@ -2,23 +2,23 @@
 
 import pandas as pd;
 
-# """ Below given file is a 1000 line of excel data.
-#     # WAP to print datas from 0-4 and 995-1000.
-#     # Wap to Describe the conclusion of the excel data.
-#     # Wap to print its Statical data of each numeric values.
-#     # Wap to check the sum of null values each column.
-# """
-# data=pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx");
-# print("Top 5 values-:");
-# print(data.head(5));
-# print("\n\n\n","Bottom 5 values-:");
-# print(data.tail(5));
-# print("\n\n\n","Conclusion of excel file-:");
-# print(data.info());
-# print("\n\n\n","Stats of each numeric column-:");
-# print(data.describe());
-# print("\n\n\n","Total number of null on each column-:");
-# print(data.isnull().sum());
+""" Below given file is a 1000 line of excel data.
+    # WAP to print datas from 0-4 and 995-1000.
+    # Wap to Describe the conclusion of the excel data.
+    # Wap to print its Statical data of each numeric values.
+    # Wap to check the sum of null values each column.
+"""
+data=pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx");
+print("Top 5 values-:");
+print(data.head(5));
+print("\n\n\n","Bottom 5 values-:");
+print(data.tail(5));
+print("\n\n\n","Conclusion of excel file-:");
+print(data.info());
+print("\n\n\n","Stats of each numeric column-:");
+print(data.describe());
+print("\n\n\n","Total number of null on each column-:");
+print(data.isnull().sum());
 
 
 
