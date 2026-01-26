45a846a0dd1405f37ad65722230caa40529195ec
Bikalpa Regmi <bikalparegmihtd3@gmail.com>
2026-01-26 13:07:00 +0545

    Learning plotting

---
+++ TwentyEight.py
@@ -0,0 +1,46 @@
+############### Box Plotting , Histogram Plotting & ViolinPlotting #################
+
+import pandas as pd;
+import matplotlib.pyplot as plt;
+
+"""
+Wap to show the multiple box plotting in a same diagram. 
+"""
+arr = [4,6,4,5,12,16,33];
+arr2 = [23,26,14,25,16,33,20];
+plt.boxplot([arr,arr2]);
+plt.show();
+
+
+"""
+Wap to apply box plotting of age and annual salary in the ESD excel file.
+"""
+data=pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx");
+plt.boxplot(data["Annual Salary"]);
+plt.show();
+plt.boxplot(data["Age"]);
+plt.show();
+    
+
+"""
+Wap to apply histogram plotting of the score in demo practice pandas file.
+"""
+data = pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/demo_pandas_practice_xslx.xlsx");
+plt.hist(data["Score"],bins=10,color="pink",edgecolor="grey");
+plt.show();
+
+"""
+WAP to perform violin plotting of salary and ethnicity in ESD excel file.
+"""
+data = pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx")
+
+groups = [group["Annual Salary"].values for name, group in data.groupby("Ethnicity")];
+
+labels = data["Ethnicity"].unique();
+
+plt.violinplot(groups);
+plt.xticks(range(1, len(labels) + 1), labels);
+plt.xlabel("Ethnicity");
+plt.ylabel("Annual Salary");
+plt.title("Violin Plot of Salary by Ethnicity");
+plt.show();
---
+++ TwentySeven.py
@@ -0,0 +1,38 @@
+################ PyChart Plotting ##################
+import pandas as pd;
+import matplotlib.pyplot as plt;
+
+"""
+Wap to make a pyChart. Use different parameters.
+"""
+sub=["BIM","BCA","BBS","BBA","BSC CSIT"];
+no_of_std=[60,40,35,55,23];
+color = ["Red","Green","Blue","Black","Purple"];
+exp = [0.1,0,0,0,0];
+plt.pie(no_of_std,labels=sub,colors=color,explode=exp,autopct="%.2f",shadow=True);
+plt.show();
+
+
+
+"""
+Wap to make pyChart of a ESD excel file. Make piechart on basis of department label and salary.
+"""
+data=pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx");
+df = pd.DataFrame(data);
+print(df.columns);
+department_Salary = df.groupby("Department")["Annual Salary"].sum();
+print(department_Salary);
+plt.pie(department_Salary.values,labels=department_Salary.index,autopct="%.2f");
+plt.show();
+
+
+
+"""
+Wap to make pyChart of a ESD excel file. Make piechart on basis of department label and number of employees.
+"""
+data=pd.read_excel("C:/Users/user/OneDrive - Hetauda School of Management and Social Sciences/Desktop/Data Analysis/Pandas Practice Excel/ESD.xlsx");
+df = pd.DataFrame(data);
+department_Jobs = df.groupby("Department")["EEID"].count();
+print(department_Jobs);
+plt.pie(department_Jobs.values,labels=department_Jobs.index,autopct="%.2f");
+plt.show();
